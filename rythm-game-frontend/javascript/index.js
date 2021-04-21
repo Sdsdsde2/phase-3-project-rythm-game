621a2cd4e259179cffd3dc4fb2b0d6aff5d7b15c
sir-udenna <udennaboy@gmail.com>
2021-04-21 15:25:09 -0500

    fixed anim frontend-update

--- rythm-game-frontend/javascript/index.js
+++ rythm-game-frontend/javascript/index.js
@@ -1,4 +1,5 @@
 let gameStarted = false
+let main = document.getElementsByClassName('main-game')[0]
 gameStarted = true
 
 document.addEventListener('keydown', logKey);
@@ -6,8 +7,10 @@ document.addEventListener('keydown', logKey);
 function logKey(e) {
     if(e.code=="ArrowUp"){
         console.log(` "${e.code}" was clicked`)
+        createCircle();
     }else if(e.code=="ArrowDown"){
         console.log(` "${e.code}" was clicked`) 
+
     }else if(e.code=="ArrowLeft"){
         console.log(` "${e.code}" was clicked`)
     }else if(e.code=="ArrowRight"){
@@ -17,6 +20,30 @@ function logKey(e) {
     }
 }
 
+function createCircle(){
+    let circle = document.createElement('div')
+    circle.id = "gamecircle"
+    circle.style.height = '100px';
+    circle.style.width = '100px';
+    circle.style.backgroundColor = '#bbb';
+    circle.style.borderRadius = '50%';
+
+    main.append(circle)
+}
+
+function myFunction() {
+    document.getElementById("gamecircle").style.animation = "mynewmove 4s 20";
+  }
 
 
-console.log('index.js loaded')
+// function createCircle(){
+//     let circle = document.createElement('div')
+//     circle.className = "a"
+//     console.log(circle.style)
+
+//     main.append(circle)
+//     // document.getElementsByClassName('a')[0].className += " b"
+// }
+
+
+console.log('index.js fully loaded')
