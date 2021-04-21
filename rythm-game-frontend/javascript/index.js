d75e8e3a2bd1ff0613519dbbd4ef09b821e0be1f
sir-udenna <udennaboy@gmail.com>
2021-04-21 10:53:51 -0500

    beg3

--- rythm-game-frontend/javascript/index.js
+++ rythm-game-frontend/javascript/index.js
@@ -6,27 +6,17 @@ document.addEventListener('keydown', logKey);
 function logKey(e) {
     if(e.code=="ArrowUp"){
         console.log(` "${e.code}" was clicked`)
-        arrowChange('arrow_up')
     }else if(e.code=="ArrowDown"){
         console.log(` "${e.code}" was clicked`) 
-        arrowChange('arrow_down')
     }else if(e.code=="ArrowLeft"){
-        console.log(` "${e.code}" was clicked`) 
-        arrowChange('arrow_left')
+        console.log(` "${e.code}" was clicked`)
     }else if(e.code=="ArrowRight"){
         console.log(` "${e.code}" was clicked`) 
-        arrowChange('arrow_right')
     }else{
         console.log('Other key was pressed') //removing after finish
     }
 }
 
-function arrowChange(element){
-    let arrow = document.getElementById(element)
-    console.log(arrow)
-    arrow.style.borderColor = "Blue"
-    arrow.classList.add('horizTranslate')
-    //
-    }
+
 
 console.log('index.js loaded')
