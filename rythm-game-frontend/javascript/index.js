2a53e148ff5ace044d8b7643b375325526facbbc
sir-udenna <udennaboy@gmail.com>
2021-04-22 01:45:20 -0500

    renovations frontend

--- rythm-game-frontend/javascript/index.js
+++ rythm-game-frontend/javascript/index.js
@@ -1,62 +1,29 @@
-let gameStarted = false
-let main = document.getElementsByClassName('main-game')[0]
-gameStarted = true
-let circleMatch;
-let score = 0;
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("index.js fully loaded")
+    startGameBtn()
+    controlsLol()
+})
 
-//remove points but keeps it from going under 0
-function removePoints(){
-if(score > 0){
-    score = score - 1
-}
-console.log(score)
-}
-
-let keysToBeClicked = ["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"]
-
-document.addEventListener('keydown', logKey);
-
-//for testing
-createCircle()
-//
-
-//used to check if the key matches the key that needs to be pressed
-function logKey(e) {
-    if(e.code==circleMatch){
-        console.log(` "${e.code}" Matched!`)
-        score = score + 1
-        createCircle()
-    }else{
-        console.log(`${e.code} Missed!`)
-        removePoints()
-        createCircle()
-    }
-    console.log(score)
+function startGameBtn(){
+    const button = document.querySelector('#start-game')
+    button.addEventListener("click",()=>{
+        hide('#mainp')
+        console.log('clicked')
+        show('.container')
+    })
 }
 
-//creates a random circle at a random spot in the box
-function createCircle(){
-    let circle = document.createElement('div')
-    circle.innerText = `${keysToBeClicked[Math.floor(Math.random()*4)+0]}` 
-    circleMatch = circle.innerText
-    circle.id = "gamecircle"
-    circle.style.height = '200px';
-    circle.style.width = '200px';
-    circle.style.backgroundColor = 'green';
-    circle.style.borderRadius = '50%';
-    circle.style.position = 'absolute'
-    circle.style.left = `${Math.floor(Math.random()*73)+1}%`
-    circle.style.top = `${Math.floor(Math.random()*50)+1}%`
-    main.append(circle)
-    //temp
-    setTimeout(() => {
-        circle.remove()
-        removePoints()
-    }, 1500);
+function show(page){
+    document.querySelector(`${page}`).hidden = false
 }
 
-function mainGame(){
-// Main game mechanics
+function hide(page){
+    document.querySelector(`${page}`).hidden = true
 }
 
-console.log('index.js fully loaded')
+function controlsLol(){
+    let lol = document.querySelector('#controls')
+    lol.addEventListener("click", ()=>{
+        lol.innerText = "Just click the key that shows up on the screen it isn't that hard"
+    })
+}
